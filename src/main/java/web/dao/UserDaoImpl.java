43864ac0d7979bfc6759cf33ff09098f4a7a005b
ksenys kom <ksenyakom@gmail.com>
2020-11-04 12:04:41 +0300

    3 commit clean up UserDaoImpl

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,8 +1,6 @@
 package web.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
@@ -12,52 +10,41 @@ import java.util.List;
 
 @Component
 @Transactional
-
 public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
     private EntityManager em;
 
-
-     @Override
- //    @Transactional
+    @Override
     public void saveUser(User user) {
-    //    em.getTransaction().begin();
         em.persist(user);
-     //   em.getTransaction().commit();
     }
 
     @Override
     public void update(long id, User userUpdated) {
-      //  em.getTransaction().begin();
-        User userToUpdate = em.find(User.class, id);
-        userToUpdate.setName(userUpdated.getName());
-        userToUpdate.setLastName(userUpdated.getLastName());
-        userToUpdate.setAge(userUpdated.getAge());
-     //   em.flush();
-    //    em.merge(userUpdated);
-    //    em.getTransaction().commit();
+        em.find(User.class, id);
+        em.merge(userUpdated);
     }
 
     @Override
     public User getById(long id) {
         User user = em.find(User.class, id);
         em.detach(user);
+
         return user;
     }
 
     @Override
     public void removeUserById(long id) {
-    //    em.getTransaction().begin();
         User user = em.find(User.class, id);
         em.remove(user);
-    //    em.getTransaction().commit();
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> users = em.createQuery("SELECT u from User u")
                 .getResultList();
+
         return users;
     }
 
